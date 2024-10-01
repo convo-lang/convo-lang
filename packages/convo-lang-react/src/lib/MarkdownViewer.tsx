34c0bf4985a9f4dbba1c605fce5afc230fdd006e
Scott Vance <scott@iyio.io>
2024-09-30 23:59:38 -0400

    - Added lazy option to MarkdownViewer

--- packages/convo-lang-react/src/lib/MarkdownViewer.tsx
+++ packages/convo-lang-react/src/lib/MarkdownViewer.tsx
@@ -1,5 +1,6 @@
 import { atDotCss } from "@iyio/at-dot-css";
 import { BaseLayoutProps, cn } from "@iyio/common";
+import { useLazyRender } from "@iyio/react-common";
 import { useEffect, useState } from "react";
 
 let md:{
@@ -20,17 +21,21 @@ const getMdAsync=async ()=>{
 export interface MarkdownViewerProps
 {
     markdown?:string|null;
+    lazy?:boolean;
 }
 
 export function MarkdownViewer({
     markdown,
+    lazy,
     ...props
 }:MarkdownViewerProps & BaseLayoutProps){
 
     const [elem,setElem]=useState<HTMLElement|null>(null);
 
+    const {show}=useLazyRender(lazy?elem:null);
+
     useEffect(()=>{
-        if(!elem){
+        if(!elem || (!show && lazy)){
             return;
         }
         if(!markdown){
@@ -50,7 +55,7 @@ export function MarkdownViewer({
             m=false;
         }
 
-    },[markdown,elem]);
+    },[markdown,elem,show,lazy]);
 
     return (
         <div ref={setElem} className={cn(style.root(null,null,props),markdownStyle.root())}/>
@@ -67,16 +72,16 @@ const style=atDotCss({name:'MarkdownViewer',css:`
 
 export const markdownStyle=atDotCss({name:'MarkdownStyle',css:`
     @.root h1{
-        margin:1rem 0;
+        margin:0 0 1rem 0;
     }
     @.root h2{
-        margin:0.7rem 0;
+        margin:0 0 0.7rem 0;
     }
     @.root h3,@.root h4,@.root h5,@.root h6{
-        margin:0.5rem 0;
+        margin:0 0 0.5rem 0;
     }
     @.root p{
-        margin:0.5rem 0;
+        margin:0 0 0.5rem 0;
     }
     @.root li{
         margin-bottom:1rem;
