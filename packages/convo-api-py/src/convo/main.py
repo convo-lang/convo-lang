47feb8e7d5a9392407381fe42c6c0cfb8dbe261b
zombie-einstein <13398815+zombie-einstein@users.noreply.github.com>
2024-10-24 22:21:34 +0100

    Init graph tables from databases

--- packages/convo-api-py/src/convo/main.py
+++ packages/convo-api-py/src/convo/main.py
@@ -3,7 +3,6 @@ import os
 from contextlib import asynccontextmanager
 from datetime import datetime
 
-import psycopg2
 from fastapi import FastAPI
 
 from . import db, rest_api
@@ -11,24 +10,6 @@ from . import db, rest_api
 log_file_path = f"./embedding_api_{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.log"
 logger = logging.getLogger(__name__)
 logging.basicConfig(filename=log_file_path, level=logging.INFO)
-
-
-# Temp connection until db conncection is refactored
-# Try and create graph, rollback if it already exists
-def create_graph(graph_name: str):
-    conn = psycopg2.connect(
-        database=os.getenv("PGDATABASE"),
-        host=os.getenv("PGHOST"),
-        user=os.getenv("PGUSER"),
-        password=os.getenv("PGPASSWORD"),
-        port=os.getenv("PGPORT"),
-    )
-    sql = f"SELECT * FROM ag_catalog.create_graph('{graph_name}');"
-
-    with conn.cursor() as curs:
-        curs.execute(sql)
-
-    conn.close()
 
 
 @asynccontextmanager
@@ -39,7 +20,8 @@ async def lifespan(app: FastAPI):
         graph_name = os.getenv("GRAPH_DB")
         assert graph_name is not None, "'GRAPH_DB' environment variable was not set"
         logger.info("Creating graph '%s' (if it does not exist)", graph_name)
-        create_graph(graph_name)
+        query = f"SELECT * FROM ag_catalog.create_graph('{graph_name}');"
+        await db.database.execute(query)
     except Exception as e:
         logger.info("Faild to create graph '%s' - reason: %s", graph_name, e)
 
