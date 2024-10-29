3174d367073d00bb742cc7d7b7ff5650948b06ab
Scott Vance <scott@iyio.io>
2024-10-28 22:31:51 -0400

    - Added a shorthand backend initialization for openai

---
+++ packages/convo-lang-openai/src/lib/initOpenAiBackend.ts
@@ -0,0 +1,21 @@
+import { openAiApiKeyParam, openaiConvoModule } from '@convo-lang/convo-lang-openai';
+import { ScopeModule, initRootScope, rootScope } from '@iyio/common';
+
+let inited=false;
+export const initOpenAiBackend=(apiKey?:string,module?:ScopeModule)=>{
+    if(inited){
+        return;
+    }
+    inited=true;
+    if(rootScope.initCalled()){
+        return;
+    }
+    initRootScope(reg=>{
+        reg.addParams({
+            [openAiApiKeyParam.typeName]:apiKey??process.env['OPENAI_API_KEY']??''
+        });
+        reg.use(openaiConvoModule);
+        reg.use(module);
+    })
+}
+initOpenAiBackend();
