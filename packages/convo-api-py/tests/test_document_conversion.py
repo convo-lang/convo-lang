2549a3eb2274b8ff9bb20a16d1fe4d54dfb05392
zombie-einstein <13398815+zombie-einstein@users.noreply.github.com>
2024-11-13 12:22:26 +0000

    Fix import bug and add test

---
+++ packages/convo-api-py/tests/test_document_conversion.py
@@ -0,0 +1,32 @@
+import tempfile
+from reportlab.pdfgen import canvas
+from convo.embeddings.types import DocumentConversionRequest
+
+from convo.embeddings.convert_document import convert_document
+
+
+def test_convert_pdf():
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        f_path = f"{tmpdir}/test.pdf"
+        o_path = f"{tmpdir}/test_out.txt"
+
+        pdf = canvas.Canvas(f_path)
+        title = "Title"
+        text = "Hello World"
+        pdf.setTitle(title)
+        pdf.drawString(100, 100, text)
+        pdf.save()
+
+        request = DocumentConversionRequest(
+            srcPath=f_path,
+            destPath=o_path,
+        )
+
+        result = convert_document(request)
+
+        assert result
+
+        with open(o_path, 'r') as f:
+            txt = f.read()
+            assert txt == "Hello World\n\n\x0c"
