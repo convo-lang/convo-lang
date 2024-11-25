992fd7bd9b03ee74112f019ce52b5d7efa252c02
Scott Vance <scott@iyio.io>
2024-11-25 00:10:53 -0500

    - only load AGE if enabled

--- packages/convo-api-py/src/convo/main.py
+++ packages/convo-api-py/src/convo/main.py
@@ -3,7 +3,11 @@ import os
 from contextlib import asynccontextmanager
 from datetime import datetime
 
-import age
+age_enabled=os.getenv("RUN_GRAPH_EMBED").lower() == "true"
+
+if age_enabled:
+    import age
+
 from convo.embeddings.types import GraphRagConfig
 from databases import Database
 from fastapi import FastAPI
@@ -21,10 +25,11 @@ logging.basicConfig(filename=log_file_path, level=logging.INFO)
 async def lifespan(app: FastAPI):
     db_url = get_db_url()
     app.state.db = Database(db_url)
-    app.state.ag = age.Age()
+    if age_enabled:
+        app.state.ag = age.Age()
     app.state.open_ai_client = AsyncOpenAI()
     app.state.graph_rag_config = GraphRagConfig()
-    app.state.RUN_GRAPH_EMBED = os.getenv("RUN_GRAPH_EMBED").lower() == "true"
+    app.state.RUN_GRAPH_EMBED = age_enabled
 
     await app.state.db.connect()
 
@@ -38,12 +43,14 @@ async def lifespan(app: FastAPI):
         logger.info("Faild to create graph '%s' - reason: %s", graph_name, e)
 
     graph_db_name = os.getenv("GRAPH_DB")
-    app.state.ag.connect(dsn=db_url, graph=graph_db_name)
+    if age_enabled:
+        app.state.ag.connect(dsn=db_url, graph=graph_db_name)
 
     yield
 
     await app.state.db.disconnect()
-    app.state.ag.close()
+    if age_enabled:
+        app.state.ag.close()
     app.state.open_ai_client.close()
 
 
