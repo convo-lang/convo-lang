e0043262bb7fd3e9cb084c99c6b475ce0e917a73
Scott Vance <scott@iyio.io>
2025-06-08 19:31:48 -0400

    - v0.7.18

--- packages/convo-lang-tools/publish.sh
+++ packages/convo-lang-tools/publish.sh
@@ -6,6 +6,12 @@ cd lsp
 ./build-lsp.sh
 cd ..
 
+# How to create a new personal access token:
+# - goto https://dev.azure.com/phillsv87/_usersSettings/tokens and sign-in with logic email
+# - Click on the user gear icon then click on Personal access token
+# - Click create new token
+# - give full access
+
 # See https://code.visualstudio.com/api/working-with-extensions/publishing-extension
 echo token=$VS_CODE_TOKEN
-vsce publish -p $VS_CODE_TOKEN
+npx --yes vsce publish -p $VS_CODE_TOKEN
