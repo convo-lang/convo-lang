776cb209eba082395c91beeb108a4e8a653b293b
Scott Vance <scott@iyio.io>
2025-08-17 00:23:15 -0400

    - open ovsx command update

--- packages/convo-lang-tools/publish-ovsx.sh
+++ packages/convo-lang-tools/publish-ovsx.sh
@@ -2,12 +2,13 @@
 set -e
 cd "$(dirname "$0")"
 
+echo 'export const extensionPublisher="convo-lang";' > lsp/client/src/build-const.ts
+sed -i '' 's@"publisher": "IYIO",@"publisher": "convo-lang",@g' package.json
+
 cd lsp
 ./build-lsp.sh
 cd ..
 
-echo 'export const extensionPublisher="convo-lang";' > lsp/client/src/build-const.ts
-sed -i 's/"publisher": "IYIO",/"publisher": "convo-lang",/g' filename.txt
 
 # How to create a new personal access token:
 # - goto https://open-vsx.org/user-settings/tokens
@@ -15,4 +16,4 @@ echo token=$OPEN_VSX_TOKEN
 npx --yes ovsx publish -p $OPEN_VSX_TOKEN
 
 echo 'export const extensionPublisher="iyio";' > lsp/client/src/build-const.ts
-sed -i 's/"publisher": "convo-lang",/"publisher": "IYIO",/g' filename.txt
+sed -i '' 's@"publisher": "convo-lang",@"publisher": "IYIO",@g' package.json
