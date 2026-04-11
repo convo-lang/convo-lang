70b60182fe43332c5edd56aec089ed7ecc2e4a0e
Scott Vance <scott@iyio.io>
2026-04-11 17:39:53 -0400

    - dropped legacy graph project

--- packages/convo-lang-legacy-graph/src/lib/convo-graph-deps.ts
+++
@@ -1,4 +0,0 @@
-import { defineService } from "@iyio/common";
-import { ConvoGraphStore } from "./convo-graph-types.js";
-
-export const convoGraphStore=defineService<ConvoGraphStore>('ConvoGraphStore');
