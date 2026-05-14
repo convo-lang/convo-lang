633516ce818bd1405a910f5f32b1ccde023b2f21
Scott Vance <scott@iyio.io>
2026-05-14 18:10:39 -0400

    - fixed getConvoDbUsingConnectionStringAsync

--- packages/convo-lang/src/lib/db/convo-db-connection-loader.ts
+++ packages/convo-lang/src/lib/db/convo-db-connection-loader.ts
@@ -2,8 +2,10 @@ import { convoDbProvider } from "../convo.deps.js";
 import { ConvoDb } from "./convo-db-types.js";
 
 export const getConvoDbUsingConnectionStringAsync=async (connection:string):Promise<ConvoDb|undefined>=>{
-    const i=connection.indexOf(':');
-    const tag=i===-1?connection:connection.substring(0,i).trim();
-    let con=i===-1?undefined:connection.substring(i+1).trim();
-    return await convoDbProvider.get(tag)?.(con);   
+    const [name,type,...args]=connection.split(':');
+    if(!type || !name){
+        return undefined;
+    }
+    const r=await convoDbProvider.get(type)?.(type,args);
+    return r?.(name);
 }
